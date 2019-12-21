e4d85f4c118b3251ed3b8211f474c9eab88c99c6
Hodaka Suzuki <altitude3190@gmail.com>
2019-12-21 19:44:51 +0900

    Update scaffold.sh

--- python/scaffold.sh
+++ python/scaffold.sh
@@ -6,8 +6,8 @@
 pipenv install
 
 # flake8のインストール
-pip install flake8
-pip install autopep8
+pip install flake8 --dev
+pip install autopep8 --dev
 
 echo 'VSCodeの設定は、https://qiita.com/psychoroid/items/2c2acc06c900d2c0c8cbを見てください'
 
